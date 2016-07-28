1bac5e1c52aa1b8d79889211a6e773401ca9c1f3
Jonathan Peppers <jonathan.peppers@hitcents.com>
2016-07-27 23:24:07 -0500

    Models

---
+++ XamSnap/XamSnap/Conversation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace XamSnap
+{
+    public class Conversation
+    {
+        public string Id { get; set; }
+
+        public string UserId { get; set; }
+
+        public string Username { get; set; }
+    }
+}
+
