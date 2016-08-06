dccf55564a40af979b7fd2f5a535aeef841795e4
Jonathan Peppers <jonathan.peppers@hitcents.com>
2016-08-06 14:50:07 -0500

    iOS - added FriendsController

---
+++ iOS/Controllers/FriendsController.cs
@@ -0,0 +1,13 @@
+using Foundation;
+using System;
+using UIKit;
+
+namespace XamSnap.iOS
+{
+    public partial class FriendsController : UITableViewController
+    {
+        public FriendsController (IntPtr handle) : base (handle)
+        {
+        }
+    }
+}
