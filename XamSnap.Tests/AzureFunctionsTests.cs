636a0848fd142e53fa57eaa030a2c6eb28b084a6
Jonathan Peppers <jonathan.peppers@hitcents.com>
2016-09-28 23:27:51 -0500

    Azure Functions - Login API is working

---
+++ XamSnap.Tests/AzureFunctionsTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace XamSnap.Tests
+{
+    [TestFixture]
+    public class AzureFunctionsTests
+    {
+        private AzureFunctionsWebService _service;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _service = new AzureFunctionsWebService();
+        }
+
+        [Test]
+        public async Task LoginNewUser()
+        {
+            var user = await _service.Login(Guid.NewGuid().ToString("N"), "Woot");
+
+            Assert.IsNotNull(user);
+        }
+
+        [Test, ExpectedException(typeof(HttpRequestException))]
+        public async Task LoginExistingUser()
+        {
+            var user = await _service.Login("Azure", "Woot");
+
+            Assert.IsNotNull(user);
+        }
+    }
+}
---
+++ XamSnap/Azure/AzureFunctionsWebService.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace XamSnap
+{
+    public class AzureFunctionsWebService : IWebService
+    {
+        private const string BaseUrl = "https://xamsnap.azurewebsites.net/api/";
+        private const string Code = "n49qfil4y79il6yezfuwyiudi6avxyn09coyk4urlfh83b7f1orrgye9uaaupq6w82vp36czyqfr";
+        private const string ContentType = "application/json";
+        private readonly HttpClient _httpClient = new HttpClient();
+
+        public Task<User> AddFriend(string userName, string friendName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Conversation[]> GetConversations(string userName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Message[]> GetMessages(string conversation)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<User> Login(string userName, string password)
+        {
+            string json = JsonConvert.SerializeObject(new
+            {
+                userName,
+                password,
+            });
+            var content = new StringContent(json);
+            content.Headers.ContentType = new MediaTypeHeaderValue(ContentType);
+
+            var response = await _httpClient.PostAsync(BaseUrl + "login?code=" + Code, content);
+            response.EnsureSuccessStatusCode();
+
+            return new User
+            {
+                Name = userName,
+                Password = password,
+            };
+        }
+
+        public Task<User> Register(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Message> SendMessage(Message message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
